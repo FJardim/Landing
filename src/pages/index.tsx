ce2e78ea38374aca643d199b96e2815e6e5715b1
ProjectsReactJs <reactjsoriginprojects@gmail.com>
2023-06-23 18:07:07 -0400

    modificacion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import { NavBar } from "../components/navBar/NavBar";
 import Nosotros from "../components/navBar/Nosotros";
 import Especialidades from "../components/navBar/Especialidades";
 import Servicios from "../components/Servicios";
+import MapContainer from "../components/Maps/MapContainer";
+import Formulario from "../components/navBar/Contacto";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,9 @@ export default function Home() {
   parraph={"Brindamos un asesoramiento notarial de forma integral, acompañando a nuestros clientes a lo largo de todo el proceso. El objetivo principal es ayudar tanto a empresas como a particulares, ofreciendo un servicio transparente, cercano, eficiente y rápido acorde a sus necesidades particulares."}
 />
       <Especialidades title={"Nuestras Especialidades"}/>
-      <Servicios/>
+      <Servicios title={'Nuestros Servicios'}/>
+      <MapContainer/>
+      <Formulario title={'Dejanos tu mensaje'}/>
     </div>
   );
 }
