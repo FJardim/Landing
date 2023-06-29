ba8d85dafd573342a3cbc4f838c982c9962960d5
ProjectsReactJs <reactjsoriginprojects@gmail.com>
2023-06-29 11:47:35 -0400

    cambios en el index linkeo de secciones de la landing

--- src/components/navBar/navBar.tsx
+++ src/components/navBar/navBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-
+import { Link as ScrollLink } from 'react-scroll';
 import logo from "../../assets/image/EscribaniaD.png";
 
 export const NavBar = () => {
@@ -11,22 +11,38 @@ export const NavBar = () => {
         <Image src={logo} className="w-48 h-20 mt-6" alt="Logo" />
         <ul className="lg:flex ml-4 hidden md:block">
           <li className="mx-4 text-white cursor-pointer">
-            <Link href="#nosotros">Nosotros</Link>
+          <ScrollLink
+              to="nosotros"
+              smooth={true}
+              duration={400}>Nosotros</ScrollLink>
           </li>
           <li className="mx-4 text-white cursor-pointer">
-            <Link href="#especialidades">Especialidades</Link>
+          <ScrollLink
+              to="especialidades"
+              smooth={true}
+              duration={400}>
+         Especialidades
+         </ScrollLink>
           </li>
           <li className="mx-4 text-white cursor-pointer">
-            <Link href="#servicios">Servicios</Link>
+          <ScrollLink
+              to="servicios"
+              smooth={true}
+              duration={400}>
+        Servicios
+        </ScrollLink>
           </li>
           <li className="mx-4 text-white cursor-pointer">
-            <Link href="/documents">Documento</Link>
+        <Link href='/documents'>Documento</Link>
           </li>
           <li className="mx-4 text-white cursor-pointer">
-            <Link href="#contacto">Contacto</Link>
+          <ScrollLink
+              to="contacto"
+              smooth={true}
+              duration={400}>Contacto</ScrollLink>
           </li>
           <li className="mx-4 text-white cursor-pointer">
-            <Link href="#marketplace-abogados">Marketplace Abogados</Link>
+          Marketplace Abogados
           </li>
         </ul>
         <button className="hidden md:block text-white mx-8 bg-main hover:text-black px-2 py-1 rounded-2xl">
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,7 @@ export default function Home() {
     <div className="">
       <Header />
       <div className="p-12 bg-slate-200">
+      <section id="nosotros">
         <Nosotros
           title={"Acerca de Nosotros"}
           subtitle={"Fundada hace más de 40 años"}
@@ -22,14 +23,22 @@ export default function Home() {
             "Brindamos un asesoramiento notarial de forma integral, acompañando a nuestros clientes a lo largo de todo el proceso. El objetivo principal es ayudar tanto a empresas como a particulares, ofreciendo un servicio transparente, cercano, eficiente y rápido acorde a sus necesidades particulares."
           }
         />
+        </section>
+        <section id="especialidades">
         <Especialidades title={"Nuestras Especialidades"} />
+      </section>
       </div>
       <div className=" bg-slate-200">
+        <section id="servicios">
         <Servicios title={"Nuestros Servicios"} />
+        </section>
         <MapContainer />
       </div>
       <div className="p-12">
+        <section id="contacto">
         <Formulario title={"Dejanos tu mensaje"} />
+        </section>
+        
       </div>
       <PublContact />
       <Footer />
